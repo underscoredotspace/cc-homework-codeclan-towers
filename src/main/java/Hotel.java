35256012e36a0e594423e2f3887f31c7d0574971
Colin Tindle <ampersand@underscore.space>
2018-05-18 17:50:30 +0100

    added addRoom method to Hotel class

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -2,4 +2,16 @@ import java.util.ArrayList;
 
 public class Hotel {
     private ArrayList<Room> rooms;
+
+    Hotel() {
+        this.rooms = new ArrayList<>();
+    }
+
+    public void addRoom(Room room) {
+        this.rooms.add(room);
+    }
+
+    public int getRoomCount() {
+        return this.rooms.size();
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -5,9 +5,30 @@ import static org.junit.Assert.assertEquals;
 
 public class HotelTest {
     private Hotel hotel;
+    private Bedroom room101;
+    private ConferenceRoom robroy;
 
     @Before
     public void before() {
         hotel = new Hotel();
+        room101 = new Bedroom("101", BedroomType.DOUBLE);
+        robroy = new ConferenceRoom("Rob Roy", 5, 100);
+    }
+
+    @Test
+    public void canGetRoomCount() {
+        assertEquals(0, hotel.getRoomCount());
+    }
+
+    @Test
+    public void canAddBedRoom() {
+        hotel.addRoom(room101);
+        assertEquals(1, hotel.getRoomCount());
+    }
+
+    @Test
+    public void canAddConferenceRoom() {
+        hotel.addRoom(robroy);
+        assertEquals(1, hotel.getRoomCount());
     }
 }
